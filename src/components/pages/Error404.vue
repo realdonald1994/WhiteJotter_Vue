a4fee56f69867b6e128d151beed0d8052bb0d63a
realdonald1994 <realdonald1994@outlook.com>
2020-06-11 18:38:23 -0400

    Add some test with jest

---
+++ src/components/pages/Error404.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="error-page">
+    <div style="padding: 100px 50px 0 0">
+      <img src="" class="error-tag">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Error404"
+  }
+</script>
+
+<style scoped>
+  .error-page {
+    background: url("../../assets/img/bg/bg2.jpg") no-repeat center;
+    margin: 0;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+  }
+</style>
